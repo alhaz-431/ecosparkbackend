62e998d7e9af5bc9f3d11900112302cd85c50c28
Alhaz <alhaz@gmail.com>
2026-04-18 15:54:31 +0600

    updated

--- src/routes/vote.routes.ts
+++ src/routes/vote.routes.ts
@@ -4,6 +4,7 @@ import { authenticate } from '../middlewares/auth';
 
 const router = Router();
 
+// এই একটি রাউট দিয়েই আপভোট এবং ডাউনভোট হবে
 router.post('/:id/vote', authenticate, voteIdea);
 
 export default router;
